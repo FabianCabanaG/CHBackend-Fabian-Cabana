25f605906ab5e8814a3418edfdda699bbcc78c1f
Fabian Cabana <fakekian@gmail.com>
2023-09-12 23:22:19 -0500

    second challenge complete, added folders for organization

---
+++ index.js
@@ -0,0 +1,46 @@
+const {ProductManager} = require('./managers/1ProductManager');
+
+const manager = new ProductManager('./CHBackend-Fabian Cabana/files/products.json');
+
+
+const env = async () => {
+    const products = await manager.getProducts();
+    console.log(products);
+
+    // const productstwo = await manager.getProductById(1);
+    // console.log(productstwo);
+
+    // const product_prueba = {
+    //     title: 'producto prueba',
+    //     description:'Este es un producto prueba',
+    //     price:200,
+    //     thumbnail:'Sin imagen',
+    //     code:'abc12355',
+    //     stock:25
+    // };
+
+    const product_prueba_2 = {
+        title: 'producto pruebaaaaaaaaaaaaaaa',
+        description:'Este es un producto pruebaaaaaaaaaaaaaaa',
+        price:200,
+        thumbnail:'Sin imagenaaaaaaaaaa',
+        code:'abc12355aaaaaaa',
+        stock:25
+    };
+    
+    // await manager.addProduct(product_prueba);
+
+    // await manager.updateProduct(1,product_prueba_2)
+
+    await manager.deleteProduct(1);
+
+    const productss = await manager.getProducts();
+    console.log(productss);
+
+}
+
+env();
+
+// manager.addProduct('ra','ra',12,'raa','rar',12);
+
+// console.log(manager.getProducts())
---
+++ managers/1ProductManager.js
@@ -0,0 +1,140 @@
+const fs = require('fs')
+
+class ProductManager {
+
+    constructor(path) {
+        // this.products = [];
+        this.path = path;
+    }
+
+    getProducts = async () => {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, 'utf-8');
+                const products = JSON.parse(data);
+                return products;
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getProductById = async (id) => {
+        try {
+
+            const checkId = id;
+            const producto_completo = await this.getProducts();
+            const productFound = producto_completo.find(element => element.id === checkId);
+            if (!productFound) {
+                console.log('Producto No registrado');
+                return
+            }
+            return productFound
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    updateProduct = async (id, objeto) => {
+        try {
+
+            const checkId = id;
+            const producto_completo = await this.getProducts();
+            const productFound = producto_completo.find(element => element.id === checkId);
+            if (!productFound) {
+                console.log('Producto No registrado');
+                return
+            }
+            // si no se da un objeto como argumento, detener la ejecucion
+            if (!objeto) {
+                console.log('se debe dar un objeto para reemplazar')
+            }
+
+            if (!objeto.title || !objeto.description || !objeto.price || !objeto.thumbnail || !objeto.code || !objeto.stock) {
+                console.log('Todos los campos del objeto deben existir')
+            }
+
+            objeto.id = checkId
+
+            let producto_completo_2 = producto_completo.filter(element => element.id !== checkId);
+
+            producto_completo_2.push(objeto);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(producto_completo_2, null, '\t'));
+
+            return producto_completo_2
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    deleteProduct = async (id) => {
+        try {
+
+            const checkId = id;
+            const producto_completo = await this.getProducts();
+            const productFound = producto_completo.find(element => element.id === checkId);
+            if (!productFound) {
+                console.log('Producto No registrado');
+                return
+            }
+
+            let producto_completo_2 = producto_completo.filter(element => element.id !== checkId);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(producto_completo_2, null, '\t'));
+
+            return producto_completo_2
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    addProduct = async (producto) => {
+        try {
+            const producto_completo = await this.getProducts()
+
+            // validar que no se repita el campo code 
+            const productCode = producto_completo.findIndex(element => element.code === producto.code);
+            // console.log(productCode)
+            if (!(productCode === -1)) {
+                console.log('Producto ya registrado');
+                return
+            }
+
+            // Continues execution after validation
+            if (producto_completo.length === 0) {
+                // console.log(this.products.length);
+                producto.id = 1;
+            } else {
+                // console.log(this.products.length);
+                producto.id = producto_completo[producto_completo.length - 1].id + 1;
+            }
+
+
+
+            producto_completo.push(producto);
+
+
+            await fs.promises.writeFile(this.path, JSON.stringify(producto_completo, null, '\t'));
+
+            return (producto_completo);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+
+
+const prueba = new ProductManager();
+
+module.exports = {
+    ProductManager
+}
